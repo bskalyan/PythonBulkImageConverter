a124bd81ab093e8c82854f59203b0cbec2423093
Enrico Tuvera Jr <teh.saber@gmail.com>
2014-12-20 11:42:56 +0800

    Changed layout so that it's a bit nicer to look at.

--- README.txt
+++ README.txt
@@ -1,21 +1,3 @@
-This directory contains version 1.0 release of the Python Bulk Image Converter.
-Copyright 2013 Enrico Tuvera Jr/cornjuliox, all rights reserved.
-
 DESCRIPTION
 ============================
-I wrote this little program specifically to deal with all the screenshots I take with Fraps while playing games. Currently, the demo version of Fraps has this restriction in which all screenshots taken are in the BMP format, which takes up a lot of space compared to other formats, especially when you set it to automatically take screenshots at regular intervals.
-
-USAGE
-============================
-1. First, select the image format you want the files to be converted to.
-2. Second, click the "Load Files" button, navigate to the appropriate directory and then shift-click the files you intend to convert.
-2a. It occurs to me that some people don't know how to select multiple files with the shift-click, so here's a quick how to: given any list of files in the
-"open file" dialog, click on the first file at the top of the list. Next, scroll down to the bottom of the list, and hold shift, and click on the last file.
-This will select all the files and folders from the beginning to the end. Entries you don't wish to include can be removed by holding shift+ctrl+clicking on each
-file you wish to remove.
-3. Verify in the main screen that all the files you've selected are correct.
-4. Press "Convert" to convert each file. The new files will be placed in same directory as the originals, albeit with a different extension.
-Use your file browser to sort through them.
-
-LICENSE
-============================
+So yeah this readme is out of date. 
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MyMenu(wx.Frame):
     # extend it to accept multiple files later.
     # start with the interface.
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title)
+        super(MyMenu, self).__init__(parent, id, title)
 
         valid_targets = ["jpg", "gif", "pcx", 
             "msp", "png", "ppm", "tiff"]
@@ -26,8 +26,7 @@ class MyMenu(wx.Frame):
         # [CONVERT]
 
         self.convert_target = None
-        self.raw_file = None
-
+        self.raw_file = []
         self.panel = wx.Panel(self, -1)
 
         # all elements will be declared in the order in which
@@ -35,7 +34,7 @@ class MyMenu(wx.Frame):
         self.intro_text = wx.StaticText(self.panel, -1, "Image Converter v 0.1")
 
         # Load files button and binding
-        self.file_convert_text = wx.StaticText(self.panel, -1, 
+        self.file_convert_text = wx.StaticText(self.panel, -1,
             "Select a file to convert.")
         self.file_text_box = wx.TextCtrl(self.panel, -1,
             "Select a file!", style=wx.TE_READONLY)
@@ -43,11 +42,11 @@ class MyMenu(wx.Frame):
         self.load_file_button.Bind(wx.EVT_BUTTON, self.SelectFileDialog)
 
         # Conversion type selection box and label
-        self.choice_list_desc = wx.StaticText(self.panel, -1, 
+        self.choice_list_desc = wx.StaticText(self.panel, -1,
             "Select format to convert to: ")
-        self.choice_list = wx.Choice(self.panel, -1, 
+        self.choice_list = wx.Choice(self.panel, -1,
             choices=valid_targets)
-        self.choice_list.Bind(wx.EVT_CHOICE, 
+        self.choice_list.Bind(wx.EVT_CHOICE,
             self.SelectFromChoice)
 
         # Convert button!
@@ -55,29 +54,22 @@ class MyMenu(wx.Frame):
         self.go_button.Bind(wx.EVT_BUTTON, self.PerformConversion)
 
         # Sizers, bitch.
-        self.master_container = wx.BoxSizer(wx.VERTICAL)
-        self.top_row = wx.BoxSizer(wx.HORIZONTAL)
-        self.mid_row = wx.BoxSizer(wx.HORIZONTAL)
-        self.mid_row2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.bot_row = wx.BoxSizer(wx.HORIZONTAL)
+        self.master_container = wx.GridBagSizer(4, 3)
 
-        self.top_row.Add(self.intro_text, 1, wx.ALIGN_LEFT, wx.EXPAND) 
+        self.master_container.Add(self.intro_text, (0,0), (1,3), wx.EXPAND)
 
-        self.mid_row.Add(self.file_convert_text, 1, wx.ALIGN_RIGHT)
-        self.mid_row.Add(self.file_text_box, 1, wx.ALIGN_RIGHT)
-        self.mid_row.Add(self.load_file_button, 1, wx.ALIGN_LEFT)
+        self.master_container.Add(self.file_convert_text, (1,0), wx.DefaultSpan, wx.EXPAND | wx.ALIGN_RIGHT)
+        self.master_container.Add(self.file_text_box, (1,1), wx.DefaultSpan, wx.EXPAND | wx.ALIGN_CENTER)
+        self.master_container.Add(self.load_file_button, (1,2), wx.DefaultSpan, wx.EXPAND | wx.ALIGN_CENTER)
 
-        self.mid_row2.Add(self.choice_list_desc, 1, wx.ALIGN_LEFT)
-        self.mid_row2.Add(self.choice_list, 1, wx.ALIGN_LEFT)
+        self.master_container.Add(self.choice_list_desc, (2,0), wx.DefaultSpan, wx.EXPAND)
+        self.master_container.Add(self.choice_list, (2,1), wx.DefaultSpan, wx.EXPAND)
 
-        self.bot_row.Add(self.go_button, 0, wx.ALIGN_CENTER)
-
-        self.master_container.Add(self.top_row, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=10)
-        self.master_container.Add(self.mid_row, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
-        self.master_container.Add(self.mid_row2, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
-        self.master_container.Add(self.bot_row, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.BOTTOM | wx.LEFT | wx.RIGHT, border=10)
+        self.master_container.Add(self.go_button, (3,0), wx.DefaultSpan, wx.EXPAND)
 
         self.panel.SetSizer(self.master_container)
+        self.master_container.Fit(self)
+        self.Centre()
 
     def UtilityInfoDialog(self, text, title):
         msg = wx.MessageDialog(self, text, title, style=wx.OK | wx.ICON_INFORMATION)
@@ -95,15 +87,16 @@ class MyMenu(wx.Frame):
                 "Select a file to convert before hitting the convert button!",
                 "Error!")
         else:
-            self.ConvertImage(self.raw_file, self.convert_target)
+            for index, image in enumerate(self.raw_file):
+                self.ConvertImage(image, self.convert_target, index)
 
     def SelectFileDialog(self, event):
         dialog = wx.FileDialog(self, message="Select Files", defaultDir=os.getcwd(),
-                           defaultFile="", style=wx.OPEN | wx.CHANGE_DIR)
+                           defaultFile="", style=wx.OPEN | wx.CHANGE_DIR | wx.MULTIPLE)
         if dialog.ShowModal() == wx.ID_OK:
-            self.raw_file = dialog.GetPath()
+            self.raw_file = dialog.GetPaths()
             if self.raw_file:
-                self.file_text_box.SetValue(self.raw_file)
+                self.file_text_box.SetValue(", ".join(self.raw_file))
             else: 
                 self.UtilityInfoDialog("File could not be loaded for some reason!",
                     "Error!")
@@ -113,7 +106,7 @@ class MyMenu(wx.Frame):
     def SelectFromChoice(self, event):
         self.convert_target = event.GetString()
 
-    def ConvertImage(self, image, convert_to):
+    def ConvertImage(self, image, convert_to, index=0):
         """ Takes two arguments, first is a path to the image you wish to convert,
         while the second is the extension that you wish to convert to. Saves the new file to
         disk at the same filepath (albeit with a different extension) but the function 
@@ -124,9 +117,10 @@ class MyMenu(wx.Frame):
         if os.path.splitext(image)[1].lower() == convert_to.lower():
             self.UtilityInfoDialog("%s is already of filetype %s" % (x, convert_to), "Error")
         else:
-            Image.open(image).save("%s-%s.%s" % (os.path.splitext(image)[0], convert_to, convert_to))
-            self.UtilityInfoDialog("Image conversion successful. Check source directory for new file.", 
-                "Success!")
+            Image.open(image).save("%s-%s-%s.%s" % (os.path.splitext(image)[0], index, convert_to, convert_to))
+        
+        self.UtilityInfoDialog("Image conversion successful. Check source directory for new file(s).", 
+                "Success!")            
 
 class MyApp(wx.App):
     def OnInit(self):
